bf375eb6eab7b4500ebe9d1a3fb7f70b5b580d71
Simon <72569478+JCMSimon@users.noreply.github.com>
2024-04-29 16:24:20 +0200

    New thumbnail stuff (#1)
    * new thumbnail gen

--- main.py
+++ main.py
@@ -1,99 +1,99 @@
-import datetime
 import os
-import time
 import threading
-from PIL import Image,ImageDraw,ImageFont
+import time
 from dotenv import load_dotenv
-
-from yt_dlp import YoutubeDL
 from googleapiclient.discovery import build
+from PIL import Image, ImageDraw, ImageFont
+from yt_dlp import YoutubeDL
+
+
 
 
 class ValorVod:
 	def __init__(self) -> None:
-		self.data = dict()
-		self.data["agents"] = ["brimstone","viper","omen","killjoy","cypher","sova","sage","phoenix","jett","reyna","raze","breach","skye","yoru","astra","kayo","chamber","neon","fade","harbor","gekko","deadlock","iso","clove"]
-		self.data["maps"] = ["bind","haven","split","ascent","icebox","breeze","fracture","pearl","lotus","sunset"]
-		# ---
-		self.processed_vids = list()
-		# Load api key from .env
+		self.data = {
+			"agents": ["brimstone", "viper", "omen", "killjoy", "cypher", "sova", "sage", "phoenix", "jett",
+					   "reyna", "raze", "breach", "skye", "yoru", "astra", "kayo", "chamber", "neon", "fade",
+					   "harbor", "gekko", "deadlock", "iso", "clove"],
+			"maps": ["bind", "haven", "split", "ascent", "icebox", "breeze", "fracture", "pearl", "lotus", "sunset"]
+		}
+		self.processed_vids = set()
+		# Load API key from .env
 		load_dotenv()
-		API_KEY:str = os.environ.get("API_KEY")
-		self.ytAPI = build('youtube', 'v3', developerKey=API_KEY)
-		self.C_ID:str = os.environ.get("C_ID") 
-		# ---
-		self.STARTUP_TIME:int = time.time()
-		
+		self.API_KEY = os.environ.get("API_KEY")
+		self.ytAPI = build('youtube', 'v3', developerKey=self.API_KEY)
+		self.C_ID = os.environ.get("C_ID")
+		self.STARTUP_TIME = time.time()
+
 	def start(self):
-		self.newVideoThread = threading.Thread(target=self.checkForNewVideos,daemon=True)
+		self.newVideoThread = threading.Thread(target=self.check_for_new_videos, daemon=True)
 		self.newVideoThread.start()
 
-	def checkForNewVideos(self):
-		request = self.ytAPI.search().list(
-		part='snippet',
-		channelId=self.C_ID,
-		order='date',
-		type='video',
-		maxResults=4,
-		)
-		response = request.execute()
-		ytData = response.get('items', [])
-		for video in ytData:
-			# if video is newer than startup and id not in processed 
-			# if int(datetime.datetime.strptime((video["snippet"]["publishTime"]),"%Y-%m-%dT%H:%M:%SZ").timestamp()) > self.STARTUP_TIME and video["id"]["videoId"] not in self.processed_vids:
-			if video["id"]["videoId"] not in self.processed_vids:
-				vidThread = threading.Thread(target=self.processVid,args=[video["id"]["videoId"]],daemon=True)
-				vidThread.start()
-				time.sleep(10)
-		time.sleep(((60 * 60) * 3) + (60 * 10))
-		self.checkForNewVideos()
+	def check_for_new_videos(self):
+		while True:
+			request = self.ytAPI.search().list(
+				part='snippet',
+				channelId=self.C_ID,
+				order='date',
+				type='video',
+				maxResults=4,
+			)
+			response = request.execute()
+			yt_data = response.get('items', [])
+			for video in yt_data:
+				if video["id"]["videoId"] not in self.processed_vids:
+					vid_thread = threading.Thread(target=self.process_vid, args=[video["id"]["videoId"]],
+												  daemon=True)
+					vid_thread.start()
+					time.sleep(10)
+			time.sleep(((60 * 60) * 3) + (60 * 10))
 
-	def processVid(self, id:str):
-		# Title
-		description = self.getVideoDescription(id)
-		description_words = set(word.lower() for word in description.replace("kay/o","kayo").split(" "))
+	def process_vid(self, id: str, download=True):
 		try:
-			player = description.split("twitch.tv/")[1].split("\n")[0].capitalize().replace("official","")
-			agent:str = [agent for agent in self.data["agents"] if agent in description_words][0].capitalize()
-			vmap:str = [vmap for vmap in self.data["maps"] if vmap in description_words][0].capitalize()
-		except Exception as  e:
-			print("skipping",id)
-			# unsupported map or agent or player didnt work, skip
-			return		
-		title= f"[VV] {player} - {vmap} - {agent}"
+			description = self.get_video_description(id)
+			description_words = set(word.lower() for word in description.replace("kay/o", "kayo").split(" "))
+			player = description.split("twitch.tv/")[1].split("\n")[0].capitalize().replace("official", "")
+			agent = [agent for agent in self.data["agents"] if agent in description_words][0].capitalize()
+			vmap = [vmap for vmap in self.data["maps"] if vmap in description_words][0].capitalize()
+		except Exception as e:
+			print(f"Skipping {id}: {e}")
+			return
+		title = f"[VV] {player} - {vmap} - {agent}"
 		# Download video
-		with YoutubeDL({'outtmpl': f"./assets/videos/{id}.mp4",}) as ydl:
-			ydl.download([f"https://youtube.com/watch?v={id}"])
+		if download:
+			with YoutubeDL({'outtmpl': f"./assets/videos/{id}.mp4"}) as ydl:
+				ydl.download([f"https://youtube.com/watch?v={id}"])
 		# Thumbnail
-		base = Image.open("./assets/thumbnail_base.png")
-		draw = ImageDraw.Draw(base)
-		font = ImageFont.truetype("./assets/fonts/Valorant Font.ttf",size=145)
-		_, _, w, h = draw.textbbox(xy=(0,0),text=player,font=font)
-		draw.text((485 - (w / 2),180),player,(235,233,226),font=font)
-		_, _, w, h = draw.textbbox(xy=(0,0),text=agent,font=font)
-		draw.text((485 - (w / 2),490),agent,(235,233,226),font=font)
-		_, _, w, h = draw.textbbox(xy=(0,0),text=vmap,font=font)
-		draw.text((485 - (w / 2),800),vmap,(235,233,226),font=font)		
-		agentImage = Image.open(f"./assets/agents/{agent.lower()}.png")
-		base.alpha_composite(agentImage,(1000,80))
-		base.save(f"./assets/vThumbnails/{id}.png")		
-		self.processed_vids.append(id)
+		map_image = Image.open(f"./assets/maps/{vmap.lower()}.png")
+		map_image.alpha_composite(Image.open("./assets/thumbnail_base.png"), (0, 0))
+		draw = ImageDraw.Draw(map_image)
+		font = ImageFont.truetype("./assets/fonts/Valorant Font.ttf", size=145)
+		for text, y_coord in zip([player, agent, vmap], [180, 495, 810]):
+			_,_,text_width,_ = draw.textbbox(xy=(0,0),text=text, font=font)
+			draw.text((485 - (text_width // 2), y_coord), text, (235, 233, 226), font=font)
+		agent_image = Image.open(f"./assets/agents/{agent.lower()}.png")
+		map_image.alpha_composite(agent_image, (map_image.width - agent_image.width - 160, 80))
+		map_image.save(f"./assets/vThumbnails/{id}.png")
+		self.processed_vids.add(id)
 		print(title)
-		base.show()
-		# upload video here once its downloaded
+		map_image.show()
+
+		# upload video
 		
-	def getVideoDescription(self,id):
+			
+	def get_video_description(self, id):
 		request = self.ytAPI.videos().list(
-		part='snippet',
-		id=id,
+			part='snippet',
+			id=id,
 		)
 		response = request.execute()
-		vidData = response.get('items', [])
-		return vidData[0]["snippet"]["description"].replace("!","")
+		vid_data = response.get('items', [])
+		return vid_data[0]["snippet"]["description"].replace("!", "")
+
 
 if __name__ == "__main__":
-	myapp = ValorVod()
-	# myapp.start()
-	myapp.processVid("MQs5Lzo13_E")
+	my_app = ValorVod()
+	# my_app.start()
+	my_app.process_vid("MQs5Lzo13_E",download=False)
 	while True:
 		time.sleep(60)
